d00817c6bb6d7333114a31799d14e2b205c3d63e
Matthew Baseman <matthew.baseman@gmail.com>
2023-03-29 16:46:04 -0700

    Functional Stack Complete

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,31 @@
 var Stack = function() {
-  var someInstance = {};
+  var counter = 0;
+  var someInstance = {
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+
   // Implement the methods below
   someInstance.push = function(value) {
+    // add value with current increment to storage
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.pop = function() {
+    if (counter > 0) {
+      var temp = storage[counter];
+      delete storage[counter];
+      counter--;
+      return temp;
+    }
+
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
