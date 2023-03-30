02a98dccfb8a8c757e106c161d033d6387a7b5a9
Matthew Baseman <matthew.baseman@gmail.com>
2023-03-29 19:41:04 -0700

    Part One of Assignment Complete

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.counterEnter = 0;
+    this.counterExit = 0;
+    this.storage = {};
   }
 
+  enqueue(value) {
+    this.storage[this.counterEnter] = value;
+    this.counterEnter++;
+  }
+
+  dequeue() {
+    if (this.counterExit < this.counterEnter) {
+      var temp = this.storage[this.counterExit];
+      delete this.storage[this.counterExit];
+      this.counterExit++;
+      return temp;
+    }
+  }
+
+  size() {
+    return this.counterEnter - this.counterExit;
+  }
 }
