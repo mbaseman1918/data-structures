ab38bc376c2c54fa379b4f3e39511ab3d065bce6
Matthew Baseman <matthew.baseman@gmail.com>
2023-03-29 19:30:34 -0700

    Pseudoclassical Complete

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.counterEnter = 0;
+  this.counterExit = 0;
+  this.storage = {};
 };
 
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.counterEnter] = value;
+  this.counterEnter++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.counterExit < this.counterEnter) {
+    var temp = this.storage[this.counterExit];
+    delete this.storage[this.counterExit];
+    this.counterExit++;
+    return temp;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.counterEnter - this.counterExit;
+};
