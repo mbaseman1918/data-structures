d94bd64365486988ac2c903b010bb6860903a854
Matthew Baseman <matthew.baseman@gmail.com>
2023-03-31 12:13:46 -0700

    Graph Completed

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,35 +1,62 @@
 
 
 // Instantiate a new graph
-var Graph = function() {
+var Graph = function(value) {
+  this.nodes = {};
+
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes[node] = {};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  var exists = false;
+  for (var key in this.nodes) {
+    if (key === node.toString()) {
+      exists = true;
+    }
+  }
+  return exists;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var key in this.nodes[node]) {
+    var targetNode = this.nodes[node][key];
+    var currentNode = this.nodes[key][node];
+    console.log('target', targetNode);
+    console.log('current connection', currentNode);
+    this.removeEdge(targetNode, currentNode);
+  }
+  delete this.nodes[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this.nodes[fromNode][toNode] !== undefined;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.nodes[fromNode][toNode] = toNode;
+  this.nodes[toNode][fromNode] = fromNode;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.nodes[fromNode][toNode];
+  delete this.nodes[toNode][fromNode];
+
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var key in this.nodes) {
+    cb(key);
+  }
 };
 
 /*
@@ -37,3 +64,12 @@ Graph.prototype.forEachNode = function(cb) {
  */
 
 
+/*
+graph = {
+  a: [b,c,d],
+  b: [a],
+  c: [a,d],
+  d: [a, c]
+}
+
+*/
